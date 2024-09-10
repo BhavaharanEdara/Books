6e2d247db664676ff027117bd89ca096a7411d20
Edara.Bhavaharan <89628831+BhavaharanEdara@users.noreply.github.com>
2024-09-10 16:14:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,28 @@ app.post("/books", async (req, res) => {
   }
 });
 
+app.put("/books/:id", async(req,res)=>{
+
+  const bookId = req.params.id
+  try{
+    const updatedBook = await Books.findByIdAndUpdate(bookId, req.body, {new: true})
+    if (!updatedBook) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.status(200).json({
+      message: "Book updated successfully",
+      book: updatedBook,
+    });
+
+  }
+  catch(error){
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+})
+
+
 app.delete("/books/:id", async (req, res) => {
   const bookId = req.params.id;
 
